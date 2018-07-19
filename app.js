755cd39e40416d9e409bc2c2f8804312c0947cd9
Neil Fitzgerald <neil.dot.fitzgerald@gmail.com>
2018-07-19 03:47:14 +0100

    Snapper

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressSession({
-  secret: 'keyboard dog',
-  resave: false,
-  saveUninitialized: false,
+    secret: 'keyboard dog',
+    resave: false,
+    saveUninitialized: false,
 }))
 
 app.use(passport.initialize())
@@ -38,56 +38,56 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.get('/login',
-  function(req, res) {
-    res.render('login')
-  }
+    function(req, res) {
+        res.render('login')
+    }
 )
 
 app.post('/login',
-  passport.authenticate('local', {
-    successRedirect: '/secret',
-    failureRedirect: '/login',
-    failureFlash: true,
-  }),
-  function(req, res) {
-    res.redirect('/')
-  }
+    passport.authenticate('local', {
+        successRedirect: '/secret',
+        failureRedirect: '/login',
+        failureFlash: true,
+    }),
+    function(req, res) {
+        res.redirect('/')
+    }
 )
 
 app.get('/logout',
-  function(req, res) {
-    req.logout()
-    res.redirect('/')
-  }
+    function(req, res) {
+        req.logout()
+        res.redirect('/')
+    }
 )
 
 app.get('/secret',
-  function(req, res) {
-    res.render('secret')
-  }
+    function(req, res) {
+        res.render('secret')
+    }
 )
 
 app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res) {
-    res.render('profile', { user: req.user })
-  }
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req, res) {
+        res.render('profile', { user: req.user })
+    }
 )
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 app.listen(3000)
